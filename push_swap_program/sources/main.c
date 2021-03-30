d6c6bc06a3116e57106a2f281671b95914ee919e
ahaddad98 <amine.haddadd@gmail.com>
2021-03-30 16:52:19 +0100

    HERE WE GO ! PUSH_SWAP MOW>>>

--- push_swap_program/sources/main.c
+++ push_swap_program/sources/main.c
@@ -6,14 +6,29 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 12:04:37 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/03/30 12:05:31 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/03/30 16:18:58 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-int main()
+int main(int ac, char **av)
 {
-    puts("HERE WE GO !");
+    t_push_swap push_swap;
+        int         i = 1;
+    int         k = 0;
+    char    *line;
+    int        ret_read;
+    push_swap.checker = 0;
+    push_swap.a = malloc(sizeof(char *) * ac);
+    while (i < ac)
+    {
+        push_swap.a[k] = ft_strdup(av[i]);
+        i++;
+        k++;
+    }
+    push_swap.a[k] = NULL;
+    push_swap.b = NULL;
+    push_swap.checker = 0;
     return (0);
 }
