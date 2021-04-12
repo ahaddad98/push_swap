b504bda9c1258cba5aebfeaec5d24b7028a96856
ahaddad98 <amine.haddadd@gmail.com>
2021-04-12 18:01:18 +0000

    error in 100 element

--- push_swap_program/sources/main.c
+++ push_swap_program/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 12:04:37 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/04/12 15:11:44 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/04/12 18:00:52 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ void tri_insert(t_push_swap *push_swap, int len)
         i++;
     }
     push_swap->mediane = 9223372036854775807;
-    if (len <= 10)
+    if (len <= 20)
     {
         push_swap->med = malloc(sizeof(char *) * 2);
         i = len / 2;
@@ -282,14 +282,28 @@ void use_mediane(t_push_swap *push_swap, int len)
         }
     }
     i = 0;
-    if (len <= 12)
+    int index = 0;
+    if (len <= 20)
     {
-        while (push_swap->a && push_swap->a[0] && (count_line(push_swap->a) > 3))
+        if (len == 5)
         {
-            push_b(push_swap);
-            puts("pb");
-            push_swap->nbr_of_inst++;
+            while (push_swap->a && push_swap->a[0] && (count_line(push_swap->a) > 3))
+            {
+                push_b(push_swap);
+                puts("pb");
+                push_swap->nbr_of_inst++;
+            }
         }
+        else
+        {
+            while (push_swap->a && push_swap->a[0] /*&& (count_line(push_swap->a) > 3)*/)
+            {
+                push_b(push_swap);
+                puts("pb");
+                push_swap->nbr_of_inst++;
+            }
+        }
+        
         i = count_line(push_swap->med) - 1;
         j = 0;
         k = 0;
@@ -298,18 +312,51 @@ void use_mediane(t_push_swap *push_swap, int len)
         {
             algo_3_elem(push_swap);
         }
-        if (count_line(push_swap->b) == 0)
+        if (count_line(push_swap->b) == 2 && !index)
         {
             if (ft_atoi(push_swap->b[0]) <  ft_atoi(push_swap->b[1]))
             {
                 swap_b(push_swap->b, push_swap);
-                puts("sa");
+                puts("sb");
             }
             while (push_swap->b && push_swap->b[0])
             {
                 push_a(push_swap);
                 puts("pa");
                 push_swap->nbr_of_inst++;
+            }
+        }
+        else
+        {
+            index = 1;
+            while (push_swap->b && push_swap->b[0])
+            {
+                int max = get_max(push_swap);
+                {
+                    len_b = count_line(push_swap->b) / 2;
+                    while (push_swap->b && ft_atoi(push_swap->b[0]) != max)
+                    {
+                        if (len_b >= get_index1(push_swap, (max)))
+                        {
+                            rotate(push_swap->b, push_swap);
+                            puts("rb");
+                            push_swap->nbr_of_inst++;
+                        }
+                        else if (len_b < get_index1(push_swap, (max)))
+                        {
+                            rot_rot(push_swap->b, push_swap);
+                            puts("rrb");
+                            push_swap->nbr_of_inst++;
+                        }
+                    }
+                    if (ft_atoi(push_swap->b[0]) == max)
+                    {
+                        push_a(push_swap);
+                        puts("pa");
+                        push_swap->nbr_of_inst++;
+                        max = get_max(push_swap);
+                    }
+                }
             }
         }
     }
